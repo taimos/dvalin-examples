1206802af39fddfc5acad0d7c9ce662d55f78836
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2018-07-09 06:58:10 +0200

    update Dvalin to 1.28

--- jaxrs-hello-world/src/test/java/de/taimos/helloworld/HelloAPITest.java
+++ jaxrs-hello-world/src/test/java/de/taimos/helloworld/HelloAPITest.java
@@ -1,22 +1,21 @@
 package de.taimos.helloworld;
 
-import org.apache.http.HttpResponse;
 import org.junit.Assert;
 import org.junit.Test;
 
 import de.taimos.daemon.log4j.Log4jLoggingConfigurer;
 import de.taimos.daemon.spring.SpringDaemonTestRunner;
 import de.taimos.dvalin.test.jaxrs.APITest;
-import de.taimos.httputils.WS;
+import de.taimos.httputils.HTTPResponse;
 
 @SpringDaemonTestRunner.RunnerConfiguration(svc = "helloworld", config = TestConfig.class, loggingConfigurer = Log4jLoggingConfigurer.class)
 public class HelloAPITest extends APITest {
     
     @Test
     public void shouldSayHello() {
-        HttpResponse httpResponse = this.request("/hello").accept("text/plain").get();
+        HTTPResponse httpResponse = this.request("/hello").accept("text/plain").get();
         this.assertOK(httpResponse);
-        Assert.assertEquals("Hello, world!", WS.getResponseAsString(httpResponse));
+        Assert.assertEquals("Hello, world!", httpResponse.getResponseAsString());
     }
     
 }
