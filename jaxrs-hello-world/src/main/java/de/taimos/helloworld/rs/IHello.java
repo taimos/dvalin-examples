86c89907bde3701aab90268ae811ec490f943469
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2018-09-23 22:12:27 +0200

    update Dvalin

--- jaxrs-hello-world/src/main/java/de/taimos/helloworld/rs/IHello.java
+++ jaxrs-hello-world/src/main/java/de/taimos/helloworld/rs/IHello.java
@@ -4,16 +4,20 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @Path("/hello")
 public interface IHello {
-    
-    @GET
-    @Produces("text/plain")
-    String sayHello();
 
-    @GET
-    @Path("/world")
-    @Produces("text/plain")
-    String sayHelloWorld();
+	@GET
+	@Produces("text/plain")
+	@Operation(summary = "Say hello")
+	String sayHello();
+
+	@GET
+	@Path("/world")
+	@Produces("text/plain")
+	@Operation(summary = "Say hello to the world")
+	String sayHelloWorld();
 
 }
--- jaxrs-hello-world/src/main/java/de/taimos/helloworld/rs/IList.java
+++ jaxrs-hello-world/src/main/java/de/taimos/helloworld/rs/IList.java
@@ -12,16 +12,21 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+
 @Path("/list")
 @Produces("application/json")
 @Consumes("application/json")
 public interface IList {
-    
-    @GET
-    List<String> getList();
 
-    @GET
-    @Path("/{id}")
-    String getItem(@PathParam("id") String id);
+	@GET
+	@Operation(summary = "Get the list of items")
+	List<String> getList();
+
+	@GET
+	@Path("/{id}")
+	@Operation(summary = "Get a single item by id")
+	String getItem(@PathParam("id") @Parameter(description = "the item id") String id);
 
 }
